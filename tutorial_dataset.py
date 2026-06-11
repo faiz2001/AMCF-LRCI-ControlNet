272bde478314f9adb4b737ab46b38818e037c486
Mohd Faiz <93815320+faiz2001@users.noreply.github.com>
2026-06-11 16:23:26 +0530

    Update tutorial_dataset.py

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -1,14 +1,13 @@
 import json
 import cv2
 import numpy as np
-
 from torch.utils.data import Dataset
-
 
 class MyDataset(Dataset):
     def __init__(self):
         self.data = []
-        with open('./training/fill50k/prompt.json', 'rt') as f:
+        self.data_root = '/data1/faiz/training/fill50k'
+        with open(f'{self.data_root}/prompt.json', 'rt') as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -17,23 +16,21 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         item = self.data[idx]
-
         source_filename = item['source']
         target_filename = item['target']
-        prompt = item['prompt']
+        prompt          = item['prompt']
 
-        source = cv2.imread('./training/fill50k/' + source_filename)
-        target = cv2.imread('./training/fill50k/' + target_filename)
+        source = cv2.imread(f'{self.data_root}/{source_filename}')
+        target = cv2.imread(f'{self.data_root}/{target_filename}')
 
-        # Do not forget that OpenCV read images in BGR order.
+        # BGR to RGB
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
 
-        # Normalize source images to [0, 1].
+        # Normalize source to [0, 1]
         source = source.astype(np.float32) / 255.0
 
-        # Normalize target images to [-1, 1].
+        # Normalize target to [-1, 1]
         target = (target.astype(np.float32) / 127.5) - 1.0
 
         return dict(jpg=target, txt=prompt, hint=source)
-
